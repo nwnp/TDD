54f85a1f28548e87d16a014f14b3101250d98260
nwnp <jjw_0418@naver.com>
2022-06-18 14:34:43 +0900

    database connection

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const PORT = 8080;
 const app = express();
+const mongoose = require("mongoose");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("db connection success");
+  })
+  .catch((err) => {
+    console.error("db connection error", err);
+  });
 
 const indexController = require("./controllers/index");
 const apiController = require("./controllers/api.controller");
