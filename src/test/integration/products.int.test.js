d4229272f327ee36ed831a62c298449b76e12320
nwnp <jjw_0418@naver.com>
2022-06-23 16:26:40 +0900

    feat: Integration Teset

--- src/test/integration/products.int.test.js
+++ src/test/integration/products.int.test.js
@@ -3,6 +3,8 @@ const app = require("../../main");
 const newProduct = require("../data/new-product.json");
 const errorData = require("../data/error-data.json");
 
+let firstProduct;
+
 it("POST /api/products", async () => {
   const response = await request(app).post("/api/product").send(newProduct);
   expect(response.statusCode).toBe(201);
@@ -27,4 +29,20 @@ it("GET /api/product", async () => {
   expect(response.body[0].name).toBeDefined();
   expect(response.body[0].description).toBeDefined();
   expect(response.body[0].price).toBeDefined();
+  firstProduct = response.body[0];
+});
+
+it("GET /api/product/:productId", async () => {
+  const response = await request(app).get("/api/product/" + firstProduct._id);
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe(firstProduct.name);
+  expect(response.body.description).toBe(firstProduct.description);
+  expect(response.body.price).toBe(firstProduct.price);
+});
+
+it("GET /api/product/:productId (doesnt exsit)", async () => {
+  const response = await request(app).get(
+    "/api/product/62b40f144699bae9886e8d55"
+  );
+  expect(response.statusCode).toBe(404);
 });
